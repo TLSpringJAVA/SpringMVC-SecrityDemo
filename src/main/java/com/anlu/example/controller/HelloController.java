ae6ee2c1f2d9e0e48775221db65c4706a4a305bf
anlu <357348329@qq.com>
2017-11-27 23:08:05 +0800

    commit

--- src/main/java/com/anlu/example/controller/HelloController.java
+++ src/main/java/com/anlu/example/controller/HelloController.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 通过配置文件的方式 整合springSecrity
+ */
 @Controller
 public class HelloController {
-
     @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
     public ModelAndView welcomePage() {
 
@@ -24,19 +26,7 @@ public class HelloController {
 
         ModelAndView model = new ModelAndView();
         model.addObject("title", "Spring Security Hello World");
-        model.addObject("message", "This is protected page - Admin Page!");
-        model.setViewName("admin");
-
-        return model;
-
-    }
-
-    @RequestMapping(value = "/dba**", method = RequestMethod.GET)
-    public ModelAndView dbaPage() {
-
-        ModelAndView model = new ModelAndView();
-        model.addObject("title", "Spring Security Hello World");
-        model.addObject("message", "This is protected page - Database Page!");
+        model.addObject("message", "This is protected page!");
         model.setViewName("admin");
 
         return model;
